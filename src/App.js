34a0c68659ad514def2fa6acbe0326ebc07b6fc5
Ana de la Fuente González <anadlfuenteglez@gmail.com>
2024-07-22 12:29:28 +0200

    Router System

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 let App= ()=> {
   return (
     <div className="App">
       <h1>Hola</h1>
+      <Routes>
+        <Route path= "/register" element={
+            <p>Register</p>
+        }></Route>
+        <Route path= "/login" element={
+            <p>Login</p>
+        }></Route>
+        <Route path= "/" element={
+            <p>Índice</p>
+        }></Route>
+      </Routes>
     </div>
   );
 }
